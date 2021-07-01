93076db6ff3855cf055abc998913907774fd2d88
satyadvaya <satyadvaya@gmail.com>
2021-06-30 18:56:57 -0700

    functional reset button

--- app.js
+++ app.js
@@ -2,29 +2,48 @@
 
 // initialize state
 const guessInput = document.getElementById('guess-input-id');
-const button = document.getElementById('button-id');
+const guessButton = document.getElementById('guess-button-id');
 const comparisonSpan = document.getElementById('comparison-span-id');
 const attemptsSpan = document.getElementById('attempts-span-id');
-
+const resetButton = document.getElementById('reset-button-id');
 
 let randomNumber = Math.ceil(Math.random() * 20);
-let remainingGuesses = 4;
+let remainingGuesses = 3;
 
 // set event listeners
-button.addEventListener('click', () => {
+guessButton.addEventListener('click', () => {
 
     let guessedNumber = Number(guessInput.value);
 
-    if (guessedNumber === randomNumber) {
-        comparisonSpan.textContent = 'You guessed correctly!';
+    if (remainingGuesses === 0) {
+        comparisonSpan.textContent = 'Game Over';
+        guessButton.disabled = true;
     }
-    else if (guessedNumber > randomNumber) {
+
+    if (guessedNumber > randomNumber) {
         comparisonSpan.textContent = 'Your guess is TOO HIGH — guess lower!';
-        remainingGuesses --;
-    } else {
+        attemptsSpan.textContent = `You have ${remainingGuesses} guesses remaining!`;
+        remainingGuesses--;
+
+    } else if (guessedNumber < randomNumber){
         comparisonSpan.textContent = 'Your guess is TOO LOW — guess higher!';
-        remainingGuesses --;
+        attemptsSpan.textContent = `You have ${remainingGuesses} guesses remaining!`;
+        remainingGuesses--;
+
+    } else {
+        comparisonSpan.textContent = 'You guessed correctly!';
+        guessButton.disabled = true;
     }
+});
+
+resetButton.addEventListener('click', () => {
+    guessButton.disabled = false;
+    randomNumber = Math.ceil(Math.random() * 20);
+    remainingGuesses = 4;
+    comparisonSpan.textContent = 'I will tell you if you need to guess higher or lower ...';
+    attemptsSpan.textContent = `You have ${remainingGuesses} guesses remaining!`;
+    guessInput.value = '';
+    remainingGuesses--;
 });
 
   // get user input
